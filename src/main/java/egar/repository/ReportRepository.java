41e87c63eb71c578a8e635d258fba7061849b53d
mlgzilla <rmlgzilla@gmail.com>
2023-11-08 18:15:42 +0300

    remove pass from properties

--- src/main/java/egar/repository/ReportRepository.java
+++ src/main/java/egar/repository/ReportRepository.java
@@ -2,6 +2,7 @@ package egar.repository;
 
 import egar.domain.report.dto.ReportDtoRead;
 import egar.domain.report.entity.Report;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public interface ReportRepository extends BaseRepository<Report, Integer> {
     ReportDtoRead findById(Integer id);
 
+    @Query("SELECT r from Report r")
     Iterable<ReportDtoRead> findAll();
 
 
